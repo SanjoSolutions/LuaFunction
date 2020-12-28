5cb09159ddd369dbb51ced66a3530e2611e7fc83
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2020-12-28 20:24:40 +0100

    Adds function "curry"

--- Function.lua
+++ Function.lua
@@ -6,6 +6,26 @@ local function partial(fn, ...)
     end
 end
 
+local function curry(fn, numberOfParameters)
+    local numberOfParametersPassed = 0
+    local arguments = {}
+
+    local curriedFunction
+    curriedFunction = function (...)
+        local passedArguments = {...}
+        numberOfParametersPassed = numberOfParametersPassed + #passedArguments
+        Array.append(arguments, passedArguments)
+        if numberOfParametersPassed == numberOfParameters then
+            return fn(unpack(arguments))
+        else
+            return curriedFunction
+        end
+    end
+
+    return curriedFunction
+end
+
 Function = {
-    partial = partial
+    partial = partial,
+    curry = curry
 }
---
+++ Function.tests.lua
@@ -0,0 +1,47 @@
+require('Array/Array')
+require('Object/Object')
+require('Function')
+
+local function areTablesEqual(a, b)
+    local aKeys = Object.keys(a)
+    local bKeys = Object.keys(b)
+    if #aKeys ~= #bKeys then
+        return false
+    end
+    local allKeys = Array.concat(aKeys, bKeys)
+    for _, key in ipairs(allKeys) do
+        if a[key] ~= b[key] then
+            return false
+        end
+    end
+
+    return true
+end
+
+local function equals(a, b)
+    if type(a) == 'table' and type(b) == 'table' then
+        return areTablesEqual(a, b)
+    else
+        -- TODO: Extend as required
+        return false
+    end
+end
+
+local function expect(value)
+    return {
+        toEqual = function (expectedValue)
+            return equals(value, expectedValue)
+        end
+    }
+end
+
+function testCurry()
+    local function test(a, b, c)
+        return {a, b, c}
+    end
+    local curriedTest = Function.curry(test, 3)
+    local result = curriedTest(1)(2)(3)
+    return expect(result).toEqual({1, 2, 3})
+end
+
+print(testCurry())
