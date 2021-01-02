33bb843ee3ffc7861c9f793c5671023a0d6a21f9
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-01-02 03:57:16 +0100

    Extracts testing functionality into Testing package

--- Function.tests.lua
+++ Function.tests.lua
@@ -1,39 +1,9 @@
 require('Array/Array')
 require('Object/Object')
+require('Testing/Testing')
 require('Function')
 
-local function areTablesEqual(a, b)
-    local aKeys = Object.keys(a)
-    local bKeys = Object.keys(b)
-    if #aKeys ~= #bKeys then
-        return false
-    end
-    local allKeys = Array.concat(aKeys, bKeys)
-    for _, key in ipairs(allKeys) do
-        if a[key] ~= b[key] then
-            return false
-        end
-    end
-
-    return true
-end
-
-local function equals(a, b)
-    if type(a) == 'table' and type(b) == 'table' then
-        return areTablesEqual(a, b)
-    else
-        -- TODO: Extend as required
-        return false
-    end
-end
-
-local function expect(value)
-    return {
-        toEqual = function (expectedValue)
-            return equals(value, expectedValue)
-        end
-    }
-end
+local expect = Testing.expect
 
 function testCurry()
     local function test(a, b, c)
