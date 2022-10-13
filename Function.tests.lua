f37ebc9b98fbf351264178ac24c505b7284f1b82
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-10-13 09:25:20 +0200

    Adds second test for curry

--- Function.tests.lua
+++ Function.tests.lua
@@ -14,4 +14,14 @@ function testCurry()
     return expect(result).toEqual({1, 2, 3})
 end
 
+function testCurry2()
+    local function test(a, b)
+        return {a, b}
+    end
+    local curriedTest = Function.curry(test, 2)
+    local result = curriedTest(1)(2)
+    return expect(result).toEqual({1, 2})
+end
+
 print(testCurry())
+print(testCurry2())
