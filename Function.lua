2be84f0fbf45378c51a66294c90609ce501e7d10
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-10-19 02:26:13 +0200

    Commit

--- Function.lua
+++ Function.lua
@@ -39,9 +39,14 @@ local function identity(value)
     return value
 end
 
+local function alwaysTrue()
+    return true
+end
+
 Function = {
     partial = partial,
     curry = curry,
     noOperation = noOperation,
-    identity = identity
+    identity = identity,
+    alwaysTrue = alwaysTrue
 }
