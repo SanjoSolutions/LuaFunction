17e833a1f4398a8b740e5457f224246ee3cf6fe2
AkiKonani <aki.konani@gmail.com>
2022-11-26 03:45:01 +0100

    Work

--- Function.lua
+++ Function.lua
@@ -1,33 +1,33 @@
 local function partial(fn, ...)
-    local partialArguments = {...}
-    return function (...)
-        return fn(unpack(partialArguments), ...)
-    end
+  local partialArguments = { ... }
+  return function(...)
+    return fn(unpack(partialArguments), ...)
+  end
 end
 
 local function curry(fn, numberOfParameters)
-    local numberOfParametersType = type(numberOfParameters)
-    if numberOfParametersType ~= 'number' then
-        print('Invalid argument: numberOfParameters. ' .. numberOfParametersType .. ' given instead of number.')
-        return
+  local numberOfParametersType = type(numberOfParameters)
+  if numberOfParametersType ~= 'number' then
+    print('Invalid argument: numberOfParameters. ' .. numberOfParametersType .. ' given instead of number.')
+    return
+  end
+
+  local numberOfParametersPassed = 0
+  local arguments = {}
+
+  local curriedFunction
+  curriedFunction = function(...)
+    local passedArguments = { ... }
+    numberOfParametersPassed = numberOfParametersPassed + #passedArguments
+    Array.append(arguments, passedArguments)
+    if numberOfParametersPassed == numberOfParameters then
+      return fn(unpack(arguments))
+    else
+      return curriedFunction
     end
+  end
 
-    local numberOfParametersPassed = 0
-    local arguments = {}
-
-    local curriedFunction
-    curriedFunction = function (...)
-        local passedArguments = {...}
-        numberOfParametersPassed = numberOfParametersPassed + #passedArguments
-        Array.append(arguments, passedArguments)
-        if numberOfParametersPassed == numberOfParameters then
-            return fn(unpack(arguments))
-        else
-            return curriedFunction
-        end
-    end
-
-    return curriedFunction
+  return curriedFunction
 end
 
 local function noOperation()
@@ -35,26 +35,26 @@ local function noOperation()
 end
 
 local function identity(value)
-    return value
+  return value
 end
 
 local function alwaysTrue()
-    return true
+  return true
 end
 
 local function isTrue(value)
-    return not not value
+  return not not value
 end
 
 local function returnValue(value)
-  return function ()
+  return function()
     return value
   end
 end
 
 local function once(fn)
   local hasBeenRun = false
-  return function (...)
+  return function(...)
     if not hasBeenRun then
       hasBeenRun = true
       return fn(...)
@@ -62,13 +62,18 @@ local function once(fn)
   end
 end
 
+local function alwaysFalse()
+  return false
+end
+
 Function = {
-    partial = partial,
-    curry = curry,
-    noOperation = noOperation,
-    identity = identity,
-    alwaysTrue = alwaysTrue,
-    isTrue = isTrue,
-    returnValue = returnValue,
-    once = once
+  partial = partial,
+  curry = curry,
+  noOperation = noOperation,
+  identity = identity,
+  alwaysTrue = alwaysTrue,
+  isTrue = isTrue,
+  returnValue = returnValue,
+  once = once,
+  alwaysFalse = alwaysFalse
 }
